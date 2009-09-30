c413a4be569ed50e7ae9f4ad1fb0e5e2102a3130
Markus Groß <mgross@informatik.uni-bremen.de>
2009-09-30 08:03:47 +0200

    Fix from Russell: updated installer.

--- Resources/uninstall.sh
+++ Resources/uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Uninstalling Safari AdBlock..."
-rm -rf "/Library/InputManagers/Safari AdBlock"
+rm -r "/Library/Application Support/SIMBL/Plugins/Safari AdBlock"
 rm -rf "$HOME/Library/Application Support/Safari AdBlock"
 defaults delete com.apple.Safari ABIsEnabled
 defaults delete com.apple.Safari ABCheckForUpdates
